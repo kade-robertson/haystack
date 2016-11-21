bf6fade2db2492423524de7fbb3950f0f6c19e33
Kade Robertson <kade.robertson96@gmail.com>
2016-11-20 21:39:46 -0500

    added debug mode

--- haystack_new.py
+++ haystack_new.py
@@ -8,12 +8,14 @@ def expand_map(m):
             m[i] += ' '
     return largest, len(m), m
 
-def interpret(prog):
+def interpret(prog, debug):
     maxr, maxc, prog = expand_map(prog)
     stack = []
     dx, dy = 1, 0 # defaults to moving right
     x, y = 0, 0
+    steps = 0
     while prog[x][y] != '|':
+        steps += 1
         char = prog[x][y]
         if char == '>':
             dx, dy = 1, 0
@@ -45,26 +47,27 @@ def interpret(prog):
                     dx, dy = good
                 else:
                     dx, dy = bad
-                
-                    
         x, y = x + dx, y + dy
         x = x % maxr
         y = y % maxc
+    if debug:
+        print('Steps made: %d'%steps)
 
-def read_prog(path):
+def read_prog(path, debug = False):
     prog = ''
     with open(path, 'r') as file:
         prog = file.read()
-    interpret(prog.split('\n'))
+    interpret(prog.split('\n'), debug)
 
 def main():
     parser = argparse.ArgumentParser(description = 'Interpreter for the Haystack programming language.')
     parser.add_argument('-f', type=str, help='Open a Snake source file.')
+    parser.add_argument('-d', action='store_true', help='Print debug information.')
     args = parser.parse_args()
     if args.f != None:
         if args.f != '':
             if os.path.isfile(args.f):
-                read_prog(args.f)
+                read_prog(args.f, args.d)
             else:
                 print('-f: Error - File path given does not exist.')
         else:
