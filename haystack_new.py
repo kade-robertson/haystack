206a38a59f5a805cb777aa7558c11553ff37558e
Kade Robertson <kade.robertson96@gmail.com>
2016-11-20 21:14:01 -0500

    added basic directional chars

--- haystack_new.py
+++ haystack_new.py
@@ -3,10 +3,25 @@ def expand_map(m):
     for i in xrange(len(m)):
         while len(m[i]) < largest:
             m[i] += [' ']
-    return m
+    return largest, len(m), m
 
-def main():
-    pass
+def main(prog):
+    maxr, maxc, prog = expand_map(prog)
+    dx, dy = 1, 0 # defaults to moving right
+    x, y = 0, 0
+    while prog[x][y] != '|':
+        char = prog[x][y]
+        if char == '>':
+            dx, dy = 1, 0
+        elif char == '<':
+            dx, dy = -1, 0
+        elif char == '^':
+            dx, dy = 0, 1
+        elif char == 'v':
+            dx, dy = 0, -1
+        x, y = x + dx, y + dy
+        x = x % maxr
+        y = y % maxc
 
 if __name__ == '__main__':
     main()
