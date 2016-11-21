cce83d0011b5f535c4907371f339be78a93cd3b2
Kade Robertson <kade.robertson96@gmail.com>
2016-11-20 21:22:49 -0500

    able to read files

--- haystack_new.py
+++ haystack_new.py
@@ -1,11 +1,14 @@
+import os
+import argparse
+
 def expand_map(m):
     largest = max(map(len, m))
     for i in xrange(len(m)):
         while len(m[i]) < largest:
-            m[i] += [' ']
+            m[i] += ' '
     return largest, len(m), m
 
-def main(prog):
+def interpret(prog):
     maxr, maxc, prog = expand_map(prog)
     dx, dy = 1, 0 # defaults to moving right
     x, y = 0, 0
@@ -22,6 +25,25 @@ def main(prog):
         x, y = x + dx, y + dy
         x = x % maxr
         y = y % maxc
+
+def read_prog(path):
+    prog = ''
+    with open(path, 'r') as file:
+        prog = file.read()
+    interpret(prog.split('\n'))
+
+def main():
+    parser = argparse.ArgumentParser(description = 'Interpreter for the Haystack programming language.')
+    parser.add_argument('-f', type=str, help='Open a Snake source file.')
+    args = parser.parse_args()
+    if args.f != None:
+        if args.f != '':
+            if os.path.isfile(args.f):
+                read_prog(args.f)
+            else:
+                print('-f: Error - File path given does not exist.')
+        else:
+            print('-f: Error - no path given.')
 
 if __name__ == '__main__':
     main()
